742be93749eb326ace7a53098e04224772774dec
Matheus Bezerra <matheus.bezerra@j4s.tech>
2022-12-29 22:07:24 -0300

    Donwload cv avaible

--- src/components/Sections/Home/components/ListButton/index.tsx
+++ src/components/Sections/Home/components/ListButton/index.tsx
@@ -6,7 +6,7 @@ import { SiGmail } from 'react-icons/si'
 export const ListButton = () => {
   return (
     <Flex mt=".4rem" gap="1rem" flexWrap={"wrap"}>
-        <Button colorScheme={"blue"}>Baixar CV</Button>
+        <Button colorScheme={"blue"} as="a" href='https://drive.google.com/file/d/16cCVrHPggukcS-IDt4CBU6_8BoBgKlay/view?usp=share_link' target={"_blank"}>Baixar CV</Button>
         <Button colorScheme='linkedin' leftIcon={<FaLinkedin />} as="a" href="https://www.linkedin.com/in/matheus-bezerra04/" target="_blank">
           Linkedin
         </Button>
